7db94f833a7f3766695da52e47b9d125736633c8
marthjod@gmail.com <marthjod@gmail.com>
2020-03-10 11:05:43 +0100

    add comments

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # bm
 
 [![Travis CI Build Status](https://travis-ci.org/marthjod/bm.svg?branch=master)](https://travis-ci.org/marthjod/bm)
-[![Coverage Status](https://coveralls.io/repos/github/marthjod/bm/badge.svg?branch=master)](https://coveralls.io/github/marthjod/bm?branch=master)
--- pkg/message/message.go
+++ pkg/message/message.go
@@ -3,30 +3,32 @@ package message
 import (
 	"bytes"
 	"encoding/binary"
-	"net"
-	"time"
 )
 
 const (
+	// KnownMagicValue see https://bitmessage.org/wiki/Protocol_specification#Message_structure.
 	KnownMagicValue = 0xE9BEB4D9
-	Version         = 3
-	VerAckString    = "verack"
+	// Version is the supported BitMessage protocol version.
+	Version = 3
+	// VerAckString is the `verack` message command string.
+	VerAckString = "verack"
 )
 
-type VarInt struct {
-	Int int
-}
+// type VarInt struct {
+// 	Int int
+// }
 
-type VarIntList struct {
-	Count  VarInt
-	Values []VarInt
-}
+// type VarIntList struct {
+// 	Count  VarInt
+// 	Values []VarInt
+// }
 
-type VarString struct {
-	Len    int
-	String string
-}
+// type VarString struct {
+// 	Len    int
+// 	String string
+// }
 
+// Message is a BitMessage message, see https://bitmessage.org/wiki/Protocol_specification.
 type Message struct {
 	Magic    uint32
 	Command  string
@@ -56,25 +58,26 @@ func (m *Message) MarshalBinary() (data []byte, err error) {
 	return buf.Bytes(), nil
 }
 
-type NetworkAddress struct {
-	Time     uint64
-	Stream   uint32
-	Services uint64
-	IPv6     net.IP
-	Port     uint16
-}
+// type NetworkAddress struct {
+// 	Time     uint64
+// 	Stream   uint32
+// 	Services uint64
+// 	IPv6     net.IP
+// 	Port     uint16
+// }
 
-type MsgVersion struct {
-	Version       int32
-	Services      uint64
-	Timestamp     time.Time
-	Receiver      NetworkAddress
-	Sender        NetworkAddress
-	Nonce         uint64
-	UserAgent     string
-	StreamNumbers VarIntList
-}
+// type MsgVersion struct {
+// 	Version       int32
+// 	Services      uint64
+// 	Timestamp     time.Time
+// 	Receiver      NetworkAddress
+// 	Sender        NetworkAddress
+// 	Nonce         uint64
+// 	UserAgent     string
+// 	StreamNumbers VarIntList
+// }
 
+// VerAck returns a valid `verack` message.
 func VerAck() *Message {
 	return &Message{
 		Magic:   KnownMagicValue,
